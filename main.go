a189402bbf69d6d925d7db2cbb24185e58c40d5b
khabibullokhon <k01346595@gmail.com>
2024-01-25 23:57:02 +0500

    telegram bot configured

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 	"github.com/joho/godotenv"
-	_ "github.com/PaulSonOfLars/gotgbot/v2"
-	_ "github.com/PaulSonOfLars/gotgbot/v2/ext"
-	_ "github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
 )
 
 var botToken string
@@ -20,5 +21,49 @@ func main() {
 	}
 
 	botToken = os.Getenv("TOKEN")
-	fmt.Println(botToken)
+	if botToken == "" {
+		panic("TOKEN environment variable is empty")
+	}
+
+	b, err := gotgbot.NewBot(botToken, nil)
+	if err != nil {
+		panic("failed to create new bot: " + err.Error())
+	}
+
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+			log.Println("an error occurred while handling update:", err.Error())
+			return ext.DispatcherActionNoop
+		},
+		MaxRoutines: ext.DefaultMaxRoutines,
+	})
+	updater := ext.NewUpdater(dispatcher, nil)
+
+	dispatcher.AddHandler(handlers.NewCommand("start", start))
+
+	err = updater.StartPolling(b, &ext.PollingOpts{
+		DropPendingUpdates: true,
+		GetUpdatesOpts: &gotgbot.GetUpdatesOpts{
+			Timeout: 9,
+			RequestOpts: &gotgbot.RequestOpts{
+				Timeout: time.Second * 10,
+			},
+		},
+	})
+	if err != nil {
+		panic("failed to start polling: " + err.Error())
+	}
+	log.Printf("%s has been started...\n", b.User.Username)
+
+	updater.Idle()
+}
+
+func start(b *gotgbot.Bot, ctx *ext.Context) error {
+	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, I'm @%s. I <b>will check your subscriptions</b>", b.User.Username), &gotgbot.SendMessageOpts{
+		ParseMode: "html",
+	})
+	if err != nil {
+		return fmt.Errorf("failed to send start message: %w", err)
+	}
+	return nil
 }
